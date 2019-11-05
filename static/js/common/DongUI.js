fa0b6fc33a1f51a4f3b9766c85f3c5e6195f0331
Git-dongbotao <dongbotao1543@163.com>
2019-11-05 16:32:24 +0800

    修改了分页器处理table，并写了注释以供学习

--- static/js/common/DongUI.js
+++ static/js/common/DongUI.js
@@ -53,6 +53,47 @@ function getFileInfo(fileElement) {
     var sizeFormat=fileSize+sizeType[i];
     return {"fileName":fileName,"fileSize":sizeFormat,"dataFor":dataFor,"fileType":fileType};
 }
+/*
+*
+* 分页器的使用说明
+* 1.此分页器只有最多是10个分页元素
+* 1.在html文件中所需添加分页器的元素下面添加 <div class="DPage" data-current="1" data-total="10" data-first="1"></div>
+*  参数说明：data-current="当前分页器选择的页面索引值"  data-total="（数据量/展示数据量）计算出一共多少页" data-first="1" 当前分页器展示分页值的第一个数值（最左面）
+* 2.在页面的js文件中绑定分页器与对应的列表元素或者表格，并根据total动态生成分页元素 dPageInitialize(elementIdName,total)
+* 3.对分页元素进行点击事件监听，进行分页，要先使用chagePage()更新页面元素再调用update(具体渲染方法自定义)，默认提供了对表格的重新渲染方法，详情看updateTableData()方法的说明
+*       例如：
+*               dPageInitialize("transaction_tale",total);
+*               $("Body").delegate("#transaction_tale .DPage-Item","click",function () {
+                        chagePage(this);
+                        var current_page=$("#transaction_tale .DPage").attr("data-current");
+                        var item_attr=["enterprise_name","product_name","region_of_origin","transaction_volume"];
+                        updateTransactionTable(current_page,10);
+                })
+               function updateTransactionTable(page_number,page_size) {
+                    $("#transaction_tale tbody").html("");
+                    var start=(page_number-1)*page_size;
+                    var end=page_number*page_size;
+                    var item_name="enterprise_name";
+                    for (var i=start;i<end;i++){
+                        $("#transaction_tale tbody").append(
+                            "<tr>\n" +
+                            "   <th>"+test_data[i][item_name]+"</th>\n" +
+                            "   <th>"+test_data[i].product_name+"</th>\n" +
+                            "   <th>"+test_data[i].region_of_origin+"</th>\n" +
+                            "   <th>"+test_data[i].transaction_volume+"</th>\n" +
+                            "   <th>"+test_data[i].transaction_spread+"</th>\n" +
+                            "   <th>"+test_data[i].participating_market_power+"</th>\n" +
+                            "   <th>"+test_data[i].settlement_power+"</th>\n" +
+                            "   <th>"+test_data[i].spread_tariff+"</th>\n" +
+                            "   <th>"+test_data[i].liquidated_damages_deviation_electric_charge_yuan+"</th>\n" +
+                            "   <th>"+test_data[i].settlement_tariff+"</th>\n" +
+                            "   <th>"+test_data[i].remarks+"</th>\n" +
+                            "</tr>"
+                        )
+                    }
+                }
+*
+*/
 //分页对外接口
 function dPageInitialize(elementIdName,total) {
     var current=$("#"+elementIdName).find(".DPage").attr("data-current");
@@ -123,4 +164,29 @@ function chagePage(thisElement) {
         $(thisElement).addClass("selected");
         $(parent).attr("data-current",html);
     }
+}
+/*
+仅针对与table的数据更新渲染
+参数
+page_number-第几个页面，一般先修改分页器内容即使用chagePage（）方法，获取所需的页面索引号
+page_size-这个页面展示数据量
+table_id_name 数据展示到哪个table中
+items 数据源
+item_attr   所要展示数据的属性名集合
+*/
+function updateTableData(page_number,page_size,table_id_name,items,item_attr) {
+    $("#"+table_id_name+" tbody").html("");
+    var start=(page_number-1)*page_size;
+    var end=page_number*page_size;
+    var html="";
+    for (var i=start;i<end;i++){
+        html+="<tr>";
+        for (var j=0;j<item_attr.length;j++){
+            html+="<td>"+items[i][item_attr[j]]+"</td>";
+        }
+        html+="</tr>";
+    }
+    console.log(html);
+    $("#"+table_id_name+" tbody").append(html);
+
 }
